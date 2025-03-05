866ff33fc499255c61531042913f5176ea004947
Emadenni <emanyele.dng86@gmail.com>
2025-03-05 12:38:26 +0100

    reinstalled dependencies for compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true, 
-    environment: 'jsdom', 
-    setupFiles: './setup-tests.ts', 
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup-tests.ts',
     coverage: {
-      provider: 'v8', 
+      provider: 'v8',  
       reporter: ['json-summary', 'lcov', 'text'], 
-      include: ['src/**/*.{ts,tsx,js,jsx}'], 
-      outputDirectory: './coverage', 
-      all: true, 
+      include: ['src/**/*.{ts,tsx,js,jsx}'],  
+      outputDirectory: './coverage',  
+      all: true,  
     },
   },
 });
