f901fdd97a0456677be8bf86b15c08f166c4df48
Emadenni <emanyele.dng86@gmail.com>
2025-03-05 12:15:01 +0100

    configurated pipeline and tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, 
+    environment: 'jsdom', 
+    setupFiles: './setup-tests.ts', 
+    coverage: {
+      provider: 'v8', 
+      reporter: ['json-summary', 'lcov', 'text'], 
+      include: ['src/**/*.{ts,tsx,js,jsx}'], 
+      outputDirectory: './coverage', 
+      all: true, 
+    },
+  },
+});
