6ce5d9117dac053396803abf5d98674d84482883
Emadenni <emanyele.dng86@gmail.com>
2025-03-05 12:51:53 +0100

    changed mocks configutation temporary

--- setup-tests.ts
+++ setup-tests.ts
@@ -1,15 +1,6 @@
-import { beforeAll, afterEach, afterAll } from "vitest"; 
-import { server } from "./src/__mocks__/server"; 
-import { cleanup } from "@testing-library/react"; 
-import '@testing-library/jest-dom'; 
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./src/__mocks__/server";
 
+beforeAll(() => server.start());
 
-beforeAll(() => server.listen());
-
-
-afterEach(() => {
-  cleanup();
-  server.resetHandlers(); 
-});
-
-afterAll(() => server.close());
+afterAll(() => server.stop());
