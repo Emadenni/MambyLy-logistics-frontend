a33bde2c75dd4462c3e74673d555c5da2ad1a72f
Emadenni <emanyele.dng86@gmail.com>
2025-03-06 23:48:10 +0100

    create routes and configured store

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Services from "./pages/Services/Services";
+import ContactUs from "./pages/ContactUs/ContactUs";
+import WorkWithUs from "./pages/WorkWithUs/WorkWithUs";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import AdminPage from "./pages/AdminPage/AdminPage";
+import PrivateRoute from "./components/PrivateRoute/ProvateRoute";
+import { useAuthStore } from './store/useAuthStore';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-     
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+        <Route path="/workWithUs" element={<WorkWithUs />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="*" element={<NotFoundPage />} />
+        <PrivateRoute path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/PrivateRoute/ProvateRoute.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+import { useAuthStore } from '../store/useAuthStore';
+
+interface PrivateRouteProps extends RouteProps {
+  element: React.ReactNode;
+}
+
+const PrivateRoute = ({ element, ...rest }: PrivateRouteProps) => {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+  return (
+    <Route
+      {...rest} //retrieves props from a normal route and spreads them to privateRoute
+      element={isAuthenticated ? element : <Redirect to="/login" />}
+    />
+  );
+};
+
+export default PrivateRoute;
