699725f500ba249b3dfab883f295617bf228f2aa
Emadenni <emanyele.dng86@gmail.com>
2025-03-12 22:22:05 +0100

    fixed layout issues

--- src/pages/Services/Services.tsx
+++ src/pages/Services/Services.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import Layout from '../../components/Layout/Layout';
-import "./services.scss"
-import ServicesCard from '../../components/ServicesCard/ServicesCard';
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import "./services.scss";
+import ServicesCard from "../../components/ServicesCard/ServicesCard";
 
 const Services = () => {
   return (
     <Layout>
-          <div className="services_container"> 
-            <section className='services_container__title-box'>
+      <div className="services_container">
+        <section className="services_container__title-box">
           <h1 className="services_container__title">Varje behov har sin lösning </h1>
           <h2 className="services_container__sub-title"> välj våra tjänster som passar dig</h2>
-          </section>
-      <ServicesCard />
+        </section>
+        <ServicesCard />
       </div>
     </Layout>
   );
-}
+};
 
 export default Services;
