733d736ae983d62daa370fcde859a22d8cf39c2e
Emadenni <emanyele.dng86@gmail.com>
2025-03-17 21:47:56 +0100

    updated logout with changing global state isAuthenticated when button is clicked

--- src/components/LogoutButton/LogoutButton.tsx
+++ src/components/LogoutButton/LogoutButton.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useAuthStore } from "../../store/useAuthStore";
 
 const LogoutButton: React.FC = () => {
   const navigate = useNavigate();
+  const setIsAuthenticated = useAuthStore((state) => state.setIsAuthenticated);
 
   const handleLogout = () => {
     sessionStorage.removeItem("token");
-
+setIsAuthenticated(false);
     navigate("/");
   };
 
