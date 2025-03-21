9f026f2befb89f05183262fca28e714ab48e358e
Emadenni <emanyele.dng86@gmail.com>
2025-03-21 23:24:24 +0100

    implemented registerAdmin successfully

--- src/components/AdminsTab/AddAdmin.tsx
+++ src/components/AdminsTab/AddAdmin.tsx
@@ -13,7 +13,10 @@ const AddAdmin: React.FC<AddAdminProps> = ({ open, onClose, onAddAdmin }) => {
   
     if (result.success) {
       setSuccessMessage("Admin added successfully!");
+  
       onAddAdmin(newAdmin);
+  
+     
       setNewAdmin({
         firstName: "",
         lastName: "",
@@ -21,104 +24,95 @@ const AddAdmin: React.FC<AddAdminProps> = ({ open, onClose, onAddAdmin }) => {
         email: "",
         profileImage: "",
       });
-      onClose();
-      alert("admin added successfully");
-      window.location.reload();
+  
+  
+      setTimeout(() => {
+        onClose(); 
+        setSuccessMessage(null); 
+      }, 3000); 
     } else {
+      
       setSuccessMessage(null);
       console.error(result.message);
     }
   };
+
   return (
     <Dialog open={open} onClose={onClose}>
-    <DialogTitle>Add Admin</DialogTitle>
-    <DialogContent>
-      {error && <Alert severity="error">{error}</Alert>}
-      {successMessage && <Alert severity="success">{successMessage}</Alert>}
-  
-      <TextField
-        label="First Name"
-        fullWidth
-        value={newAdmin.firstName}
-        onChange={(e) => handleChange(e, "firstName")}
-        sx={{ marginBottom: 2 }}
-        error={Boolean(fieldErrors.firstName)}
-        helperText={fieldErrors.firstName}
-      />
-      <TextField
-        label="Last Name"
-        fullWidth
-        value={newAdmin.lastName}
-        onChange={(e) => handleChange(e, "lastName")}
-        sx={{ marginBottom: 2 }}
-        error={Boolean(fieldErrors.lastName)}
-        helperText={fieldErrors.lastName}
-      />
-      <TextField
-        label="Email"
-        fullWidth
-        value={newAdmin.email}
-        onChange={(e) => handleChange(e, "email")}
-        sx={{ marginBottom: 2 }}
-        error={Boolean(fieldErrors.email)}
-        helperText={fieldErrors.email}
-      />
-      <TextField
-        label="Password"
-        fullWidth
-        value={newAdmin.password}
-        onChange={(e) => handleChange(e, "password")}
-        sx={{ marginBottom: 2 }}
-        error={Boolean(fieldErrors.password)}
-        helperText={fieldErrors.password}
-      />
-  
+      <DialogTitle>Add Admin</DialogTitle>
+      <DialogContent>
+        {error && <Alert severity="error">{error}</Alert>}
+        {successMessage && <Alert severity="success">{successMessage}</Alert>}
 
-      <input
-        accept="image/*"
-        id="profile-image"
-        type="file"
-        hidden
-        onChange={handleFileChange}
-      />
-      <label htmlFor="profile-image">
-        <Button variant="contained" component="span" sx={{ marginBottom: 2 }}>
-          Upload Profile Image
+        <TextField
+          label="First Name"
+          fullWidth
+          value={newAdmin.firstName}
+          onChange={(e) => handleChange(e, "firstName")}
+          sx={{ marginBottom: 2 }}
+          error={Boolean(fieldErrors.firstName)}
+          helperText={fieldErrors.firstName}
+        />
+        <TextField
+          label="Last Name"
+          fullWidth
+          value={newAdmin.lastName}
+          onChange={(e) => handleChange(e, "lastName")}
+          sx={{ marginBottom: 2 }}
+          error={Boolean(fieldErrors.lastName)}
+          helperText={fieldErrors.lastName}
+        />
+        <TextField
+          label="Email"
+          fullWidth
+          value={newAdmin.email}
+          onChange={(e) => handleChange(e, "email")}
+          sx={{ marginBottom: 2 }}
+          error={Boolean(fieldErrors.email)}
+          helperText={fieldErrors.email}
+        />
+        <TextField
+          label="Password"
+          fullWidth
+          value={newAdmin.password}
+          onChange={(e) => handleChange(e, "password")}
+          sx={{ marginBottom: 2 }}
+          error={Boolean(fieldErrors.password)}
+          helperText={fieldErrors.password}
+        />
+
+        <input accept="image/*" id="profile-image" type="file" hidden onChange={handleFileChange} />
+        <label htmlFor="profile-image">
+          <Button variant="contained" component="span" sx={{ marginBottom: 2 }}>
+            Upload Profile Image
+          </Button>
+        </label>
+
+        {newAdmin.profileImage && (
+          <div>
+            <img
+              src={newAdmin.profileImage}
+              alt="Profile Preview"
+              style={{
+                width: "100px",
+                height: "100px",
+                objectFit: "cover",
+                marginTop: "10px",
+              }}
+            />
+          </div>
+        )}
+      </DialogContent>
+
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Cancel
         </Button>
-      </label>
-
-      {newAdmin.profileImage && (
-        <div>
-          <img
-            src={newAdmin.profileImage}
-            alt="Profile Preview"
-            style={{
-              width: '100px',
-              height: '100px',
-              objectFit: 'cover',
-              marginTop: '10px',
-            }}
-          />
-        </div>
-      )}
-    </DialogContent>
-  
-   {/*  {Object.values(fieldErrors).some((err) => err) && (
-      <Alert severity="error" sx={{ marginTop: 2 }}>
-        Please correct the errors in the form before submitting.
-      </Alert>
-    )}
-   */}
-    <DialogActions>
-      <Button onClick={onClose} color="primary">
-        Cancel
-      </Button>
-      <Button onClick={handleAddNewAdmin} color="primary" variant="contained">
-        Add Admin
-      </Button>
-    </DialogActions>
-  </Dialog>
-  
+        <Button onClick={handleAddNewAdmin} color="primary" variant="contained">
+          Add Admin
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
