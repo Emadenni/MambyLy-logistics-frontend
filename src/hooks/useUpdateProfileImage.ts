7397538e3545da4e7a3004d533811fe75a93cff1
Emadenni <emanyele.dng86@gmail.com>
2025-03-28 23:40:13 +0100

    removed logs after debug

--- src/hooks/useUpdateProfileImage.ts
+++ src/hooks/useUpdateProfileImage.ts
@@ -9,26 +9,20 @@ const useUpdateProfileImage = () => {
   const uploadImage = async (file: File, adminId: string) => {
     setIsUploading(true);
     setError(null);
-  
-    console.log("Uploading file:", file.name, file.type, file.size);
-  
+
     try {
       const reader = new FileReader();
       reader.onloadend = async () => {
         const base64Image = reader.result as string;
-  
-        console.log("Base64 image preview:", base64Image.substring(0, 100)); // Stampiamo solo i primi 100 caratteri
-  
+
         const requestData = {
           base64Image: base64Image,
           mimetype: file.type,
         };
-  
-        console.log("Sending request with data:", requestData);
-  
+
         const token = sessionStorage.getItem("token");
         const apiUrl = `${import.meta.env.VITE_API_BASE_URL}/admin/updateProfileImage/${adminId}`;
-  
+
         const response = await fetch(apiUrl, {
           method: "PUT",
           headers: {
@@ -37,28 +31,24 @@ const useUpdateProfileImage = () => {
           },
           body: JSON.stringify(requestData),
         });
-  
-        console.log("Response status:", response.status);
-  
+
         if (response.status === 401) {
           useAuthStore.getState().handleUnauthorized();
         }
-  
+
         const data = await response.json();
-        console.log("Response data:", data);
-  
+
         if (!response.ok) {
           throw new Error(`Image upload failed with status: ${response.status}`);
         }
-  
+
         if (data.imageUrl) {
-          console.log("New image URL:", data.imageUrl);
           setImageUrl(data.imageUrl);
         } else {
           throw new Error("Unexpected response format: imageUrl not found");
         }
       };
-  
+
       reader.readAsDataURL(file);
     } catch (err: any) {
       console.error("Upload error:", err.message);
@@ -67,7 +57,7 @@ const useUpdateProfileImage = () => {
       setIsUploading(false);
     }
   };
-  
+
   return { isUploading, error, imageUrl, uploadImage };
 };
 
