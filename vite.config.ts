bb0cab65db45fc379ce94a3715fc90259a5e8a07
Emadenni <emanyele.dng86@gmail.com>
2025-03-30 00:42:52 +0100

    updated vite config ts to read different env

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteMockServe } from "vite-plugin-mock";
 
-export default defineConfig({
-  plugins: [react(), viteMockServe()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    coverage: {
-      provider: "istanbul",
-      reporter: ["text", "json", "html"],
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react(), viteMockServe()],
+    define: {
+      'process.env': process.env, 
+      'import.meta.env.MODE': mode, 
     },
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      coverage: {
+        provider: "istanbul",
+        reporter: ["text", "json", "html"],
+      },
+      // Disabilita i test in produzione
+      enabled: !isProduction,
+    },
+  };
 });
