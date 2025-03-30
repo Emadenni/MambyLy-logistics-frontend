b3ad8208201bb6ce5faad92e1e58893464d2e86c
Emadenni <emanyele.dng86@gmail.com>
2025-03-30 01:42:29 +0100

    last changes about vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteMockServe } from "vite-plugin-mock";
 
-export default defineConfig({
-  plugins: [react(), viteMockServe()],
-  define: {
-    
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    coverage: {
-      provider: "istanbul",
-      reporter: ["text", "json", "html"],
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react(), viteMockServe()],
+    define: {
+   
     },
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      coverage: {
+        provider: "istanbul",
+        reporter: ["text", "json", "html"],
+      },
+      enabled: !isProduction, 
+    },
+  };
 });
