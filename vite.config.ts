4912a1f97b7c904698bdeb7f3928bc30eb845bdc
Emadenni <emanyele.dng86@gmail.com>
2025-03-30 01:00:04 +0100

    debugging environment issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 import { viteMockServe } from "vite-plugin-mock";
 
 export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
+  const isProduction = mode === "production"; 
 
   return {
     plugins: [react(), viteMockServe()],
     define: {
-      'process.env': process.env, 
-      'import.meta.env.MODE': mode, 
+     
+      "import.meta.env.MODE": mode,  
     },
     test: {
       globals: true,
@@ -18,8 +18,7 @@ export default defineConfig(({ mode }) => {
         provider: "istanbul",
         reporter: ["text", "json", "html"],
       },
-      // Disabilita i test in produzione
-      enabled: !isProduction,
+      enabled: !isProduction, 
     },
   };
 });
