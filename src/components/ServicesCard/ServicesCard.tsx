9418d3295b2be4d7f82f5bf7bfd09820fb1b772c
Emadenni <emanyele.dng86@gmail.com>
2025-04-03 14:38:17 +0200

    continuing changing style to services page

--- src/components/ServicesCard/ServicesCard.tsx
+++ src/components/ServicesCard/ServicesCard.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, } from "react";
+import { Link } from "react-router-dom";
 import { ServicesCardProps } from "../../types/common";
 import IconCards from "../IconCards/IconCards";
 import "./servicesCard.scss";
+import CTA from "../Cta/Cta";
 
 const ServicesCard: React.FC<ServicesCardProps> = ({ title, shortDescription, image, id, children }) => {
   const [isVisible, setIsVisible] = useState(false);
@@ -21,7 +23,16 @@ const ServicesCard: React.FC<ServicesCardProps> = ({ title, shortDescription, im
       <div className="service_card__content">
         <h3>{title}</h3>
         <p>{shortDescription}</p>
-        <div className="service_card__cta">{children}</div>
+        <div>{children}</div>
+        <Link to="/kontaktaOss">
+        <CTA
+              text="KONTAKTA OSS"
+              backgroundColor="#91ceee;"
+              color="rgba(66, 165, 245)"
+              hoverBackgroundColor="#fff"
+              hoverColor="#DBD714"
+            />
+            </Link>
       </div>
 
       <div className="service_card__icons">
