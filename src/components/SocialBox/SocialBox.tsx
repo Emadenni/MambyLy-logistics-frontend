952bff374ddb558b027e806fa87a230e13ac7dc3
Emadenni <emanyele.dng86@gmail.com>
2025-04-15 15:22:50 +0200

    optimized pages images

--- src/components/SocialBox/SocialBox.tsx
+++ src/components/SocialBox/SocialBox.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./socialBox.scss";
 import facebook_icon from "../../assets/images/socials/facebook_icon.png";
 import instagram_icon from "../../assets/images/socials/instagram_icon.png";
@@ -8,39 +7,32 @@ import tiktok_icon from "../../assets/images/socials/tiktok_icon.png";
 import whatsapp_icon from "../../assets/images/socials/whatsapp_icon.png";
 import viber_icon from "../../assets/images/socials/viber_icon.png";
 
-const SocialBox: React.FC = (props: Props) => {
+const SocialBox: React.FC = () => {
   return (
     <div className="socials_box__container">
-      <Link to="/">
-        {" "}
-        {/* fill link  */}
+      <a href="https://www.facebook.com/profile.php?id=61565198694451" aria-label="Facebook">
         <img src={facebook_icon} alt="facebook_icon" className="social_icon" />
-      </Link>
-      <Link to="/">
-        {" "}
-        {/* fill link  */}
+      </a>
+      <a href="https://www.instagram.com/mamby.ly.se/" aria-label="Instagram">
         <img src={instagram_icon} alt="instagram_icon" className="social_icon extra" />
-      </Link>
-      <Link to="/">
-        {" "}
-        {/* fill link  */}
+      </a>
+      <a href="https://www.linkedin.com/company/104626842/" aria-label="LinkedIn">
         <img src={linkedin_icon} alt="linkedin_icon" className="social_icon" />
-      </Link>
-      <Link to="/">
-        {" "}
-        {/* fill link  */}
+      </a>
+      <a
+        href="https://wa.me/46722116422?text=Hej!%20Jag%20besökte%20din%20webbplats%20och%20vill%20veta%20mer!"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="WhatsApp"
+      >
         <img src={whatsapp_icon} alt="whatsapp_icon" className="social_icon" />
-      </Link>
-      <Link to="/">
-        {" "}
-        {/*  fill link  */}
+      </a>
+      <a href="https://www.tiktok.com/@mambyly" aria-label="TikTok">
         <img src={tiktok_icon} alt="tiktok_icon" className="social_icon extra" />
-      </Link>
-      <Link to="/">
-        {" "}
-        {/* fill link  */}
+      </a>
+      <a href="viber://chat?number=%2B46722116422">
         <img src={viber_icon} alt="viber_icon" className="social_icon" />
-      </Link>
+      </a>
     </div>
   );
 };
