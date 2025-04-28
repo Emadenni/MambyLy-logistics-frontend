490a2fa44ef4f27193a1f744996cae10d829ef3f
Emadenni <emanyele.dng86@gmail.com>
2025-04-28 22:38:32 +0200

    edited video component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,47 +1,17 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./hero.scss";
 import hero_img from "../../assets/video-placeholder.webp"; // Immagine di placeholder
 import hero_video from "../../assets/video/hero-video.mp4"; // Video MP4 ottimizzato
 
 const Hero: React.FC = () => {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [isBrowser, setIsBrowser] = useState(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Se siamo nel browser, aggiorniamo lo stato
-      setIsBrowser(true);
+    if (videoRef.current) {
+      videoRef.current.play();
     }
   }, []);
-
-  useEffect(() => {
-    if (!isBrowser) return; // Assicurati che sia eseguito solo nel browser
-
-    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting && videoRef.current) {
-          videoRef.current.play(); // Avvia il video quando entra nel viewport
-        } else if (videoRef.current) {
-          videoRef.current.pause(); // Pausa il video quando non è visibile
-        }
-      });
-    };
-
-    const observer = new IntersectionObserver(handleIntersection, {
-      threshold: 0.5, // Inizia a caricare il video quando è visibile per almeno il 50%
-    });
-
-    if (videoRef.current) {
-      observer.observe(videoRef.current);
-    }
-
-    return () => {
-      if (videoRef.current) {
-        observer.unobserve(videoRef.current);
-      }
-    };
-  }, [isBrowser]);
 
   return (
     <div className="hero" data-testid="home-page">
@@ -52,24 +22,21 @@ const Hero: React.FC = () => {
         className={`hero_img ${isLoaded ? "hidden" : ""}`}
       />
 
-      {/* Video ottimizzato */}
-      {isBrowser && (
-        <video
-          ref={videoRef}
-          className={`hero_video ${isLoaded ? "loaded" : ""}`}
-          loop
-          muted
-          autoPlay
-          playsInline
-          preload="auto" // Pre-carica il video
-          onLoadedData={() => setIsLoaded(true)} // Segna il video come caricato
-          poster={hero_img} // Immagine di placeholder
-          role="video"
-        >
-          {/* Video MP4 ottimizzato */}
-          <source src={hero_video} type="video/mp4" />
-        </video>
-      )}
+      {/* Video */}
+      <video
+        ref={videoRef}
+        className={`hero_video ${isLoaded ? "loaded" : ""}`}
+        loop
+        muted
+        autoPlay
+        playsInline
+        preload="auto"
+        onLoadedData={() => setIsLoaded(true)}
+        poster={hero_img}
+        role="video"
+      >
+        <source src={hero_video} type="video/mp4" />
+      </video>
     </div>
   );
 };
