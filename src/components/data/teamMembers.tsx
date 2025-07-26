20f5d9a396ff77782b087e6875d268f6c4e1d2e5
Emadenni <emanyele.dng86@gmail.com>
2025-07-26 23:40:16 +0200

    added sales team member

--- src/components/data/teamMembers.tsx
+++ src/components/data/teamMembers.tsx
@@ -1,5 +1,6 @@
 import EmanuelePic from "../../assets/images/profileEmanuele.webp"
 import MarijaPic from "../../assets/images/marijaPic.webp"
+import MohannadPic from "../../assets/images/mohannad_qrh_bateq.webp"
 
 export const teamMembersData = [
     {
@@ -13,5 +14,11 @@ export const teamMembersData = [
       role: "Fullstack-utvecklare",
       email: "it-digital@mambylysolutions.se",
       imageUrl:  EmanuelePic,
+    },
+    {
+      name: "Mohannad Qrh Bateq",
+      role: "Growth Assistant",
+      email: "leads@mambylysolutions.se",
+      imageUrl:  MohannadPic,
     },
   ];
